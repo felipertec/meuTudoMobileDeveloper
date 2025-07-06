839f10cb9575649266957c1d3f828db5330af864
felipertec <felipe.rtec@gmail.com>
2025-07-06 18:29:41 -0300

    Adicionado a funcao que verifica o vencedor da corrida

--- MarioKartProject/src/index.js
+++ MarioKartProject/src/index.js
@@ -109,11 +109,27 @@ async function playRaceEngine(character1, character2){
         }
 
         console.log("-------------------------------------------------------------------------------")
+
     }
+}
+
+async function declareWinner(character1, character2){
+    console.log("🏁 Resultado Final da Corrida");
+    console.log(`${character1.NOME}: ${character1.PONTOS} ponto(s)`);
+    console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)`);
+
+    if(character1.PONTOS > character2.PONTOS)
+        console.log(`\n${character1.NOME} venceu a corrida! Parabéns 🏆`);
+    else if(character2.PONTOS > character1.PONTOS)
+        console.log(`\n${character2.NOME} venceu a corrida! Parabéns 🏆`);
+    else
+        console.log("A corrida terminou empatada")
+    
 }
 
 (async function main(){
     console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando ... \n`);
     await playRaceEngine(player1, player2);
+    await declareWinner(player1, player2)
 })()
 
