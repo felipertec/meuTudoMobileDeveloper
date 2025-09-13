983ac426c8a68ac7c19c4c7c853b38016eba0775
felipertec <felipe.rtec@gmail.com>
2025-09-13 18:26:59 -0300

    Subindo o projeto da champions

---
+++ projeto-champions/src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import router from "./routes/router";
+
+function createApp(){
+    const app = express();
+
+    app.use(express.json());
+
+    app.use("/api", router)
+
+    return app;
+}
+
+
+
+export default createApp;
